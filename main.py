9c0c53e983104ee6f33fa3c8d2aa72c0ec020186
Peter Willemsen <peter@codebuffet.co>
2021-04-17 22:20:00 +0200

    seems to be working? Still havent tried to compile anything with it yet

--- main.py
+++ main.py
@@ -5,20 +5,43 @@ import pathlib
 import subprocess
 import textwrap
 from distutils.dir_util import copy_tree
+import toml
 
 # Local stuff
 from patcher import patch_crate
 
+def fix_version(v):
+    split = v.split(".")
+    if len(split) == 1:
+        return v + ".0.0"
+    if len(split) == 2:
+        return v + ".0"
+    return v
+
 def main():
-    registry_path = sys.argv[1]
-    crate = sys.argv[2]
-    version = sys.argv[3]
-    output_folder = sys.argv[4]
-    tmp = tempfile.mkdtemp()
-    pathlib.Path(os.path.join(tmp, "src")).mkdir(parents=True, exist_ok=True)
+    crate = sys.argv[1]
+    version = sys.argv[2]
+    output_folder = sys.argv[3]
+
+    def patch_toml(toml_path):
+        with open(toml_path, mode='r+') as f:
+            cargo_toml = f.read()
+            toml_dict = toml.loads(cargo_toml)
+            for k in toml_dict['dependencies']:
+                if k == "no-std-compat":
+                    continue
+                toml_dict['dependencies'][k]['path'] = os.path.join(output_folder, f"{k}-{toml_dict['dependencies'][k]['version']}")
+            f.seek(0, 0)
+            f.write(toml.dumps(toml_dict))
+            
+    # tmp = tempfile.mkdtemp()
+    tmp = "/home/peter/poop"
+    print("tmp", tmp)
+    rust_project_path = os.path.join(tmp, "rust_project")
+    pathlib.Path(os.path.join(rust_project_path, "src")).mkdir(parents=True, exist_ok=True)
     print(f"Making new project depending on crate '{crate} v{version}' to download the crates required...")
     
-    with open(os.path.join(tmp, "Cargo.toml"), "w") as f:
+    with open(os.path.join(rust_project_path, "Cargo.toml"), "w") as f:
         f.write(textwrap.dedent("""
         [package]
         edition = "2018"
@@ -29,22 +52,36 @@ def main():
         rune = "0.9.0"
         """))
 
-    with open(os.path.join(tmp, "src", "main.rs"), "w") as f:
+    with open(os.path.join(rust_project_path, "src", "main.rs"), "w") as f:
         f.write(textwrap.dedent("""
         fn main() {
             println!("I'm just a stub!");
         }
         """))
 
-    p = subprocess.Popen(["cargo", "run"], cwd=tmp)
+    cargo_project_folder = os.path.join(tmp, "rust_project")
+    cargo_root_folder = os.path.join(tmp, 'cargo_root')
+    pathlib.Path(cargo_root_folder).mkdir(parents=True, exist_ok=True)
+    rust_env = {**os.environ, 'CARGO_HOME': cargo_root_folder}
+    
+    p = subprocess.Popen(["cargo", "run"], cwd=cargo_project_folder, env=rust_env)
     p.wait()
     
-    # Start with the first package, 
-    # copy it to the output directory and do the patching
-    crate_folder = f"{crate}-{version}"
-    crate_copy = os.path.join(output_folder, crate_folder)
-    copy_tree(os.path.join(registry_path, crate_folder), crate_copy)
-    patch_crate(crate_copy)
+    registry_path = os.path.join(cargo_root_folder, 'registry', 'src', 'github.com-1ecc6299db9ec823')
+    dependency_folders = os.listdir(registry_path)
+
+    for dependency_folder in dependency_folders:
+        print(f"Patching {dependency_folder}...")
+        dependency_path = os.path.join(registry_path, dependency_folder)
+        patch_crate(dependency_path)
+        
+        # Patch the dependencies to link to local (patched) dependencies only
+        patch_toml(os.path.join(dependency_path, "Cargo.toml"))
+        
+    print("Done! Add:")
+    main_crate_path = os.path.join(registry_path, f"{crate}-{version}")
+    print(f"{crate} = {{ path = \"{main_crate_path}\" }}")
+    print("To your dependencies and hope for the best!")
 
 if __name__ == "__main__":
     main()
--- patcher.py
+++ patcher.py
@@ -1,26 +1,32 @@
 import os
 import re
 import glob
+import toml
 
 std_compat_ver = "0.4.1"
-std_compat_dependency = f"""
-[dependencies.no-std-compat]
-version = "{std_compat_ver}"
-features = [ "alloc" ]
-"""
 
 def patch_crate(crate_folder):
     # Patches this folder according to https://crates.io/crates/no-std-compat
     with open(os.path.join(crate_folder, 'Cargo.toml'), mode='r+') as f:    
         cargo_toml = f.read()
+        toml_dict = toml.loads(cargo_toml)
+        if not "dependencies" in toml_dict:
+            toml_dict["dependencies"] = {}
+            
+        toml_dict["dependencies"]["no-std-compat"] = {
+            "version": std_compat_ver,
+            "features": ["alloc"]
+        }
         f.seek(0, 0)
-        f.write(cargo_toml + "\n" + std_compat_dependency)
+        f.write(toml.dumps(toml_dict))
+        f.truncate()
         del cargo_toml
 
     with open(os.path.join(crate_folder, 'src', 'lib.rs'), mode='r+') as f:    
         lib_rs = f.read()
         f.seek(0, 0)
         f.write("#![no_std]\nextern crate no_std_compat as std;\n" + lib_rs)
+        f.truncate()
         del lib_rs
 
     for src_filename in glob.iglob(os.path.join(crate_folder, 'src/**/*.rs'), recursive=True):
@@ -29,3 +35,4 @@ def patch_crate(crate_folder):
             f.seek(0, 0)
             line = "use std::prelude::v1::*;"
             f.write(line + '\n' + content)
+            f.truncate()
